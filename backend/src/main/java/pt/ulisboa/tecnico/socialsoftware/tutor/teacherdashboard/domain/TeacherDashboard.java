7e90e2df17d1d6dfcf8e716e34e8a8a6ec5a922e
PedromcaMartins <pedromcamartins@tecnico.ulisboa.pt>
2023-03-01 15:58:38 +0000

    feat (ESA_1.1): Implement domain

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/domain/TeacherDashboard.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/teacherdashboard/domain/TeacherDashboard.java
@@ -1,11 +1,15 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.domain;
 
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage;
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException;
 import pt.ulisboa.tecnico.socialsoftware.tutor.execution.domain.CourseExecution;
 import pt.ulisboa.tecnico.socialsoftware.tutor.impexp.domain.DomainEntity;
 import pt.ulisboa.tecnico.socialsoftware.tutor.impexp.domain.Visitor;
 import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.Teacher;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -20,6 +24,10 @@ public class TeacherDashboard implements DomainEntity {
 
     @ManyToOne
     private Teacher teacher;
+
+    // baseed on knowledge from studentDashBoard
+    @OneToMany(mappedBy = "teacherDashboard", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<StudentStats> studentsStats = new ArrayList<>();
 
     public TeacherDashboard() {
     }
@@ -53,6 +61,19 @@ public class TeacherDashboard implements DomainEntity {
     public void setTeacher(Teacher teacher) {
         this.teacher = teacher;
         this.teacher.addDashboard(this);
+    }
+
+    public void addStudentStats(StudentStats newStudentStats){
+        if (studentsStats.stream()
+                .anyMatch(StudentStats -> StudentStats.getCourseExecution().getId()
+                        .equals(newStudentStats.getCourseExecution().getId()))) {
+            throw new TutorException(ErrorMessage.DUPLICATE_STUDENT_STATS);
+        }
+        studentsStats.add(newStudentStats);
+    }
+
+    public List<StudentStats> getStudentsStats() {
+        return this.studentsStats;
     }
 
     public void accept(Visitor visitor) {
