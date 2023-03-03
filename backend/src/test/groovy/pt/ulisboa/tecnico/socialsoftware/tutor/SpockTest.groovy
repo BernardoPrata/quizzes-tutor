b0a0a56689e01808e12b7f2bd4205e5703705b45
Inês Duarte <inesmquirinorosapereiraduarte@tecnico.ulisboa.pt>
2023-03-03 01:20:35 +0000

    feat (ESA 1.1): Implement Tests

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/SpockTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/SpockTest.groovy
@@ -54,6 +54,7 @@ import pt.ulisboa.tecnico.socialsoftware.tutor.utils.DateHandler
 import pt.ulisboa.tecnico.socialsoftware.tutor.utils.Mailer
 import spock.lang.Shared
 import spock.lang.Specification
+import pt.ulisboa.tecnico.socialsoftware.tutor.teacherdashboard.repository.StudentStatsRepository
 
 import java.time.LocalDateTime
 
@@ -294,6 +295,10 @@ class SpockTest extends Specification {
 
     @Autowired
     ReplyRepository replyRepository
+
+    @Autowired
+    StudentStatsRepository studentStatsRepository
+
 
     @Autowired
     DemoUtils demoUtils
