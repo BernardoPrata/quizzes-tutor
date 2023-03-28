94dec604734648e4d3057c01d7cd14639dc3f196
Bernardo Prata <bernardo.almeida.prata@tecnico.ulisboa.pt>
2023-03-28 21:49:51 +0100

    feat (ESQ 3.1): Add current Course Executions Quizzes Stats

--- frontend/src/models/dashboard/TeacherDashboard.ts
+++ frontend/src/models/dashboard/TeacherDashboard.ts
@@ -4,12 +4,20 @@ export default class TeacherDashboard {
   numStudentsOver3quizes!: number;
   numStudentsOver75perc!: number;
 
+  numberOfQuizzes!: number;
+
+  uniqueQuizzesSolved!: number;
+
+  averageSolvedQuizes!: number;
   constructor(jsonObj?: TeacherDashboard) {
     if (jsonObj) {
       this.id = jsonObj.id;
       this.numberOfStudents = jsonObj.numberOfStudents;
       this.numStudentsOver3quizes = jsonObj.numStudentsOver3quizes;
       this.numStudentsOver75perc = jsonObj.numStudentsOver75perc;
+      this.numberOfQuizzes = jsonObj.numberOfQuizzes;
+      this.uniqueQuizzesSolved = jsonObj.uniqueQuizzesSolved;
+      this.averageSolvedQuizes = jsonObj.averageSolvedQuizes;
     }
   }
 }
