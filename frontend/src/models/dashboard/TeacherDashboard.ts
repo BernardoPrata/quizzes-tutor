67c75b94d5b947459cc2e547b5d48402311cb741
Inês Duarte <inesmquirinorosapereiraduarte@tecnico.ulisboa.pt>
2023-04-12 17:03:02 +0100

    fix (ESA 3.1): adding executionyears to TeacherDashboard.ts

--- frontend/src/models/dashboard/TeacherDashboard.ts
+++ frontend/src/models/dashboard/TeacherDashboard.ts
@@ -1,8 +1,8 @@
 export default class TeacherDashboard {
   id!: number;
-  numberOfStudents!: number;
-  numStudentsOver3quizes!: number;
-  numStudentsOver75perc!: number;
+  numberOfStudents!: number[]; 
+  numStudentsOver3quizes!: number[];
+  numStudentsOver75perc!: number[];
 
   constructor(jsonObj?: TeacherDashboard) {
     if (jsonObj) {
@@ -10,6 +10,7 @@ export default class TeacherDashboard {
       this.numberOfStudents = jsonObj.numberOfStudents;
       this.numStudentsOver3quizes = jsonObj.numStudentsOver3quizes;
       this.numStudentsOver75perc = jsonObj.numStudentsOver75perc;
+      this.executionYears = jsonObj.executionYears;
     }
   }
 }
